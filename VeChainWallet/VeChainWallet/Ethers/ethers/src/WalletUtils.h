81eb4785d07e27c8da66010331b867d111f36a41
tom.zeng <tom.zeng@vechain.com>
2018-09-14 16:10:59 +0800

    add demo code

---
+++ VeChainWallet/VeChainWallet/Ethers/ethers/src/WalletUtils.h
@@ -0,0 +1,36 @@
+//
+//  WalletUtils.h
+//  ethers
+//
+//  Created by 曾新 on 2018/8/12.
+//  Copyright © 2018年 Ethers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Address.h"
+#import "Signature.h"
+#import "Transaction.h"
+#import "Account.h"
+#import "SecureData.h"
+
+@interface WalletUtils : NSObject
+
++ (void)creatWalletWithPassword:(NSString *)password
+                      callBack:(void(^)(Account *account)) block;
+
++ (BOOL)isValidMnemonicPhrase: (NSString*)phrase;
+
+
++ (void)decryptSecretStorageJSON: (NSString*)json
+                                password: (NSString*)password
+                                callback: (void (^)(Account *account, NSError *NSError))callback;
+
++ (Address*)verifyMessage: (NSData*)message
+                signature: (Signature*)signature;
+
++ (void)sign: (NSData*)message
+    keystore: (NSString*)json
+    password: (NSString*)password
+       block:(void (^)(Signature *signature))block;
+
+@end
