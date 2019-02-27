cb57be5fc0c2e5b15932862f7bec08fd4dbc7cea
chaohu <chao.hu@vechain.com>
2019-02-27 15:05:43 +0800

    完善注释跟细节

--- wallet/Wallet/Modules/WalletDetail/WalletDetailVC.m
+++ wallet/Wallet/Modules/WalletDetail/WalletDetailVC.m
@@ -15,58 +15,95 @@
 #import "WalletCoverView.h"
 #import "WalletServerDetailVC.h"
 #import "WalletAddServerVC.h"
+#import "WalletSdkMacro.h"
+
 
 @interface WalletDetailVC ()<UISearchBarDelegate>
 {
-    NSString *_blockHost;
+    NSString *_blockHost;  /* The main network environment of block */
 }
-@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
-@property (weak, nonatomic) IBOutlet UILabel *vetAmountLabel;
-@property (weak, nonatomic) IBOutlet UILabel *vthoAmountLabel;
-@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
-@property (nonatomic, copy)NSString *vetAmount;
-@property (nonatomic, copy)NSString *vthoAmount;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;       /* The wallet address you created */
+@property (weak, nonatomic) IBOutlet UILabel *vetAmountLabel;     /* The VET label */
+@property (weak, nonatomic) IBOutlet UILabel *vthoAmountLabel;    /* The VTHO label */
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;      /* Search bar control */
+
+@property (copy, nonatomic) NSString *vetAmount;    /* The VET balance of wallet address */
+@property (copy, nonatomic) NSString *vthoAmount;   /* The VTHO balance of wallet address */
 
 @end
 
 @implementation WalletDetailVC
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self setNetworkEnvironmentHost];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.searchBar.delegate = self;
-
-    NSDictionary *currentWallet = [[NSUserDefaults standardUserDefaults]objectForKey:@"currentWallet"];
-    self.addressLabel.text = currentWallet[@"address"];
-    
-    [self.vetImageView setImage:[UIImage imageNamed:@"VET"]];
-    [self.vthoImageView setImage:[UIImage imageNamed:@"VTHO"]];
-    
     [self initView];
-    [self setHost];
-    
-    NSString *version = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleVersion"];
-    version = [NSString stringWithFormat:@"版本号：(%@)",version];
-    
-    UILabel *versionLabel = [[UILabel alloc]initWithFrame:CGRectMake(20, 450, 150, 30)];
-    versionLabel.text = version;
-    [self.view addSubview:versionLabel];
 }
 
-- (void)initView
-{
+
+/* Config subviews and load it */
+- (void)initView {
+   
+    // Set right bar buttonItem
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc]initWithTitle:@"选择网络"
                                                                  style:UIBarButtonItemStyleDone
                                                                 target:self
-                                                                action:@selector(selectNET)];
+                                                                action:@selector(selectTheMainNetworkEnvironment)];
     self.navigationItem.rightBarButtonItem = rightItem;
-    self.searchBar.text = @"https://appwallet.oss-cn-shanghai.aliyuncs.com/testJS/test.html";
-//    self.searchBar.text = @"https://appwallet.oss-cn-shanghai.aliyuncs.com/testJS/dist/index.html#/test";
+    
+    /*
+     Set up the search engine
+     
+     Test_Html is "https://appwallet.oss-cn-shanghai.aliyuncs.com/testJS/test.html"，
+     Test_Main_Page is "https://appwallet.oss-cn-shanghai.aliyuncs.com/testJS/dist/index.html#/test",
+
+     These pages are some of the trade methods used to quickly access contracts, Which network environment
+     they work in depends on how you set it up.
+     You can choose our test network, main network and your custom network. We have declarations it
+     in the file of WalletSdkMacro.h.
+     test network："https://vethor-node-test.vechaindev.com"
+     main network："https://vethor-node-vechaindev.com"     
+     */
+    self.searchBar.delegate = self;
+    self.searchBar.text = Test_Html;
+    self.searchBar.text = Test_Main_Page;
+    
+    
+    // Set the VET and VTHO logo
+    [self.vetImageView setImage:[UIImage imageNamed:@"VET"]];
+    [self.vthoImageView setImage:[UIImage imageNamed:@"VTHO"]];
+    
+    
+    /* Show the wallet address you created */
+    NSDictionary *currentWallet = [[NSUserDefaults standardUserDefaults]objectForKey:@"currentWallet"];
+    self.addressLabel.text = currentWallet[@"address"];
+    
+    
+    // Show the demo version infomation
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleVersion"];
+    version = [NSString stringWithFormat:@"版本号：(%@)",version];
+    CGFloat y = ScreenH -  kNavigationBarHeight;
+    UILabel *versionLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, y, ScreenW - 40, 30)];
+    versionLabel.text = version;
+    versionLabel.font = [UIFont systemFontOfSize:15.0];
+    versionLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:versionLabel];
 }
 
-- (void)selectNET
-{
+
+/*
+  Displays a selection of network controls，then you can change the main network environment
+  or add what you custom network environment.
+*/
+- (void)selectTheMainNetworkEnvironment {
+    
     WalletCoverView *coverView = [self.view viewWithTag:90];
     if (!coverView) {
         coverView = [[WalletCoverView alloc]initWithFrame:self.view.frame];
@@ -79,6 +116,7 @@
             if ([netName containsString:@"自定义"]) {
                 WalletAddServerVC *detailVC = [[WalletAddServerVC alloc]init];
                 [self.navigationController pushViewController:detailVC animated:YES];
+                
             }else{
                 WalletServerDetailVC *detailVC = [[WalletServerDetailVC alloc]init];
                 [detailVC netName:netName netUrl:netUrl];
@@ -88,23 +126,17 @@
     }
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    
-    [self setHost];
-}
 
-- (void)setHost
-{
-    NSDictionary *dictCurrentNet = [[NSUserDefaults standardUserDefaults]objectForKey:@"CurrentNet"];
+/* Change the main network environment or add what you custom network environment. */
+- (void)setNetworkEnvironmentHost{
+    NSDictionary *dictCurrentNet = [[NSUserDefaults standardUserDefaults] objectForKey:@"CurrentNet"];
     
-    if (dictCurrentNet) {
+    if (dictCurrentNet) { // Set to the main network of your choice.
         _blockHost = dictCurrentNet[@"serverUrl"];
         self.title = dictCurrentNet[@"serverName"];
         
-    }else{
-        _blockHost = @"https://vethor-node-test.vechaindev.com";
+    }else{ // THe default Boloc Host.
+        _blockHost = Test_BlockHost;
         self.title =  @"（测试）";
         
         NSMutableDictionary *serverDict = [NSMutableDictionary dictionary];
@@ -116,27 +148,36 @@
     self.vetAmountLabel.text = @"";
     
     [self getVETBalance];
-    
     [self getVTHOBalance];
 }
 
-- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
-{
+
+#define UISearchBarDelegate
+
+/*
+   When you click search button, then you can access some pages that it has some of the trade methods used to quickly access contracts,
+   they work in the main network environment or what your choice.
+ */
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     
     NSString *url = searchBar.text;
     WebViewVC *webVC = [[WebViewVC alloc]initWithURL:url];
     [self.navigationController pushViewController:webVC animated:YES];
 }
 
-- (void)getVETBalance
-{
-    NSString *urlString = [NSString stringWithFormat:@"%@/accounts/%@",_blockHost,self.addressLabel.text];
+
+/*
+ Get the VET balance from network environment, '_blockHost' is a network variable. Which network environment
+ they work in depends on how you set it up.
+*/
+- (void)getVETBalance {
+    NSString *urlString = [NSString stringWithFormat:@"%@/accounts/%@", _blockHost, self.addressLabel.text];
     AFHTTPSessionManager *httpManager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:urlString]];
     [httpManager GET:urlString
           parameters:nil
             progress:nil
-             success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject)
-     {
+             success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject){
+                 
          NSDictionary *dictResponse = (NSDictionary *)responseObject;
          NSString *amount = dictResponse[@"balance"];
          BigNumber *bigNumberCount = [BigNumber bigNumberWithHexString:amount];
@@ -149,16 +190,21 @@
          }
          self.vetAmountLabel.text = coinAmount;
          self.vetAmount = amount;
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error)
-     {
-        
-        
+                 
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error){
     }];
 }
 
-- (void)getVTHOBalance
-{
-    NSString *urlString = [_blockHost stringByAppendingString:@"/accounts/0x0000000000000000000000000000456e65726779"] ;
+
+/*
+  Get the VTHO balance from network environment, '_blockHost' is a network variable . Which network environment
+  they work in depends on how you set it up.
+  'Contract_Address' is a fixed contract address. It is declarationed in the file of WalletSdkMacro.h.
+  Contract_Address : '0x0000000000000000000000000000456e65726779'
+*/
+- (void)getVTHOBalance {
+    NSString *contractAddress = [NSString stringWithFormat:@"/accounts/%@", Contract_Address];
+    NSString *urlString = [_blockHost stringByAppendingString:contractAddress];
 
     NSMutableDictionary *dictParm = [NSMutableDictionary dictionary];
     [dictParm setObject:[self tokenBalanceData:self.addressLabel.text] forKey:@"data"];
@@ -167,8 +213,10 @@
     AFHTTPSessionManager *httpManager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:urlString]];
     httpManager.requestSerializer = [AFJSONRequestSerializer serializer];
     
-    [httpManager POST:urlString parameters:dictParm progress:^(NSProgress * _Nonnull uploadProgress) {
-    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    [httpManager POST:urlString
+           parameters:dictParm
+             progress:nil
+              success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         
         NSDictionary *dictResponse = (NSDictionary *)responseObject;
         NSString *amount = dictResponse[@"data"];
@@ -189,41 +237,42 @@
 }
 
 
-//查询thor 余额
-- (NSString *)tokenBalanceData:(NSString *)toAddress
-{
+/*
+  This methor is used to splicing header information.
+  The total length of the header is 34 and the rule is: 0x + methorID + twentyfour 0's.
+  MethorID is an 8-length fixed value used to access methods on a contract.
+*/
+- (NSString *)tokenBalanceData:(NSString *)toAddress {
     if ([[toAddress lowercaseString] hasPrefix:@"0x"]) {
         toAddress = [toAddress stringByReplacingOccurrencesOfString:@"0x" withString:@""];
     }
-    NSString *head = @"0x70a08231000000000000000000000000";
+    NSString *head = @"0x70a08231000000000000000000000000"; // 34  methorID + 后面自动补24个0
     NSString *data = [NSString stringWithFormat:@"%@%@",head,toAddress];
     return data;
 }
 
-- (void)tableView:(UITableView *)tableView didselect:(NSIndexPath *)indexPath
-{
-    
-}
-- (IBAction)enterMoreInfo:(id)sender
-{
+
+/* Enter the address detail ViewControll and see more infomation. */
+- (IBAction)enterMoreInfo:(id)sender {
     WalletMoreInfoVC *moreInfoVC = [[WalletMoreInfoVC alloc]init];
     [self.navigationController pushViewController:moreInfoVC animated:YES];
 }
 
-- (IBAction)enterTransfer:(id)sender
-{
+/* Enter the transfer ViewControll. */
+- (IBAction)enterTransfer:(id)sender {
     WalletTransferVC *transfer = [[WalletTransferVC alloc]init];
     
     UIButton *btn = (UIButton *)sender;
-    if (btn.tag == 10) { //vet transfer
+    if (btn.tag == 10) { // vet transfer
         transfer.isVET = YES;
         transfer.amount = _vetAmount;
-    }else if (btn.tag == 20){ //vtho transfer
-         transfer.isVET = NO;
+        
+    }else if (btn.tag == 20){ // vtho transfer
+        transfer.isVET = NO;
         transfer.amount = _vthoAmount;
     }
+    
     [self.navigationController pushViewController:transfer animated:YES];
-
 }
 
 @end
