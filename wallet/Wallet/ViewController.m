99425d23b06c45f4226273cfb1a19fd2152e08e8
chaohu <chao.hu@vechain.com>
2019-02-27 15:03:56 +0800

    修复控制台报错

--- wallet/Wallet/ViewController.m
+++ wallet/Wallet/ViewController.m
@@ -25,6 +25,17 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
+    NSDictionary *currentWallet = [[NSUserDefaults standardUserDefaults]objectForKey:@"currentWallet"];;
+    
+    if (currentWallet) { //有钱包，直接去详情
+        
+        WalletDetailVC *detailVC = [[WalletDetailVC alloc]init];
+        
+        UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:detailVC];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [self.navigationController presentViewController:nav animated:YES completion:NULL];
+        });
+    }
 }
 
 - (IBAction)touchMe:(UIButton *)sender
@@ -49,7 +60,7 @@
         {
 
             WalletDetailVC *detailVC = [[WalletDetailVC alloc]init];
-              [self.navigationController pushViewController:detailVC animated:YES];
+            [self.navigationController pushViewController:detailVC animated:YES];
 //            UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:detailVC];
 //            [self presentViewController:nav animated:YES completion:NULL];
             
@@ -73,28 +84,5 @@
             break;
     }
 }
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    
-    NSDictionary *currentWallet = [[NSUserDefaults standardUserDefaults]objectForKey:@"currentWallet"];;
-
-    if (currentWallet) { //有钱包，直接去详情
-
-        WalletDetailVC *detailVC = [[WalletDetailVC alloc]init];
-//        [self.navigationController pushViewController:detailVC animated:YES];
-
-        UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:detailVC];
-        [self presentViewController:nav animated:YES completion:NULL];
-
-    }
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
