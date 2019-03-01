d58785e9d09ceffcc528677f734ad786f22df0c3
chaohu <chao.hu@vechain.com>
2019-03-01 11:06:03 +0800

    规范格式

--- wallet/Wallet/Modules/Node/WalletChooseNetworkView.h
+++ wallet/Wallet/Modules/Node/WalletChooseNetworkView.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WalletChooseNetworkView : UIView<UITableViewDataSource,UITableViewDelegate>
+@interface WalletChooseNetworkView : UIView
 
 @property(nonatomic,copy)void (^block)(NSString *netName, NSString *netUrl);
 
