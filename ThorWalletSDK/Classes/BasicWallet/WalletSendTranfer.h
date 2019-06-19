c403674f6aa295bf56142ccaa5df1b705450c57e
zengxin <tom.zeng@vechain.com>
2019-06-19 10:17:27 +0800

    update test case

--- ThorWalletSDK/Classes/BasicWallet/WalletSendTranfer.h
+++ ThorWalletSDK/Classes/BasicWallet/WalletSendTranfer.h
@@ -35,7 +35,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface WalletSendTranfer : UIView
+@interface WalletSendTranfer : NSObject
 
 + (void)signAndSendTransfer:(WalletTransactionParameter *)paramModel
                    keystore:(NSString *)keystore
